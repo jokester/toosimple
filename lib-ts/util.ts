d61d65c22440408a62aa805acb4476eedc9f9eef
Wang Guan <momocraft@gmail.com>
2017-03-14 00:53:38 +0900

    add Deferred<T>

--- lib-ts/util.ts
+++ lib-ts/util.ts
@@ -50,3 +50,55 @@ export namespace Promisify {
         return toPromise2<A1, A2, void>(fun);
     }
 }
+
+/**
+ * Deferred: a wrapper for Promise that exposes fulfill / reject
+ */
+export class Deferred<T> {
+    private readonly _promise: Promise<T>;
+    private readonly _fulfill: (v: T) => void;
+    private readonly _reject: (e: any) => void;
+    readonly resolved = false;
+
+    constructor(private readonly strict = false) {
+        const self = this as any;
+        this._promise = new Promise((fulfill, reject) => {
+            self._fulfill = (v: T) => {
+                fulfill(v);
+                self.resolved = true;
+            };
+            self._reject = (e: any) => {
+                reject(e);
+                self.resolved = true;
+            };
+        });
+    }
+
+    toPromise() {
+        return this._promise;
+    }
+
+    follow(pv: PromiseLike<T>) {
+        pv.then(this._fulfill, this._reject);
+    }
+
+    /**
+     * NOTE v must be a value
+     * @param v the value
+     */
+    fulfill(v: T) {
+        if (this.strict && this.resolved) {
+            throw new Error("already resolved");
+        } else {
+            this._fulfill(v);
+        }
+    }
+
+    reject(e: any) {
+        if (this.strict && this.resolved) {
+            throw new Error("already resolved");
+        } else {
+            this._reject(e);
+        }
+    }
+}
